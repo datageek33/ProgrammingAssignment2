ea81032ab6f23a22e5fbd079ef78e7250a26b8e6
datageek33 <datageek33@users.noreply.github.com>
2015-01-23 22:36:34 -0800

    updating for assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## This functon creates a special 'matrix' object that 
+## can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    print(environment())
+    evn <- environment()
+    print(parent.env(evn))
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setmatrix <- function(solve) m <<- solve
+    #setmatrix <- function(solve) m <<- solve
+    getmatrix <- function() m
+    getevn<- function() environment()
+    list(set = set, get = get,
+         setinversematrix = setmatrix,
+         getmatrix = getmatrix,
+         getevn = getevn)
+  }
 
-}
-
-
-## Write a short comment describing this function
+##This function computes the inverse of the special
+## 'matrix' returned by makeCacheMatrix above
+## if the inverse has already been calculated and the
+## matrix has not changed, then cacheSolve should
+## return the inverse from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x'
+  
+  #attempt to get the inverted matrix
+  m <- x$getmatrix() 
+  
+  #if the inverted matrix is available, return
+  if(!is.null(m)){
+    print("found cached matrix")
+    return (m)
+  }
+  #else get the matrix
+  newmatrix <-x$get()
+  #invert the matrix
+  m<-getInverseMatrix(newmatrix)
+  #cache the new matrix
+  x$setmatrix(m)
+  m
+  
 }
+
+##this function returns the inverse of a matrix
+getInverseMatrix <- function(y){
+  print("solving")
+  solve(y) %*% y
+} 
+
+
